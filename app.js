44026e4ad4269981c1c2541deeda7a6c4888c4ba
Haodri <haodri@icloud.com>
2026-07-07 14:31:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const places = [
     { name: "Naturhistorisches Museum", lat: 52.27529138046867, lng: 10.529079268744166, image: "images/Bilder-Kachel/Naturhistorisches Museum.jpg" },
     { name: "Altstadtmarkt", lat: 52.2643, lng: 10.5241, image: "images/Bilder-Kachel/Atelier.jpg" },
     { name: "Wendenring", lat: 52.2587, lng: 10.5301, image: "images/Bilder-Kachel/Rednerpult Prinzenpark.jpeg" },
-    { name: "Kaiserstraße", lat: 52.2670, lng: 10.5357, image: "images/Bilder-Kachel/Fliegerhorst.png" }
+    { name: "Kaiserstraße", lat: 52.2670, lng: 10.5357, image: "images/Bilder-Kachel/stig.jpg" }
 ];
 
 function updateOverlay() {
